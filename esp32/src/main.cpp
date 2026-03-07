ea98c101ea099ab38eb1126c8d8725709a2bd645
Gavin P <gavinjpeeler@gmail.com>
2026-03-06 23:37:53 -0800

    Specify pins

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -23,6 +23,21 @@
 #include <AnimatedGIF.h>
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
+#define R1_PIN 42
+#define G1_PIN 41
+#define B1_PIN 40
+#define R2_PIN 38
+#define G2_PIN 39
+#define B2_PIN 37
+#define A_PIN  45
+#define B_PIN  36
+#define C_PIN  48
+#define D_PIN  35
+#define E_PIN  21 
+#define LAT_PIN 47
+#define OE_PIN  46
+#define CLK_PIN 2
+
 #define FILESYSTEM LittleFS
 #define FORMAT_LITTLEFS_IF_FAILED true
 
@@ -216,6 +231,8 @@ void setup() {
   // mxconfig.gpio.e = 18;
   // mxconfig.clkphase = false;
   // mxconfig.driver = HUB75_I2S_CFG::FM6126A;
+  
+  HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN};
 
   // Display Setup
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
